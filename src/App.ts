cadb2ff8560314e4be0b1cc0affb5b1502ba07f9
rockcoolt <rockcoolt@gmail.com>
2017-05-14 15:50:37 +0200

    update

--- src/App.ts
+++ src/App.ts
@@ -4,7 +4,6 @@ import * as logger        from 'morgan';
 import * as bodyParser    from 'body-parser';
 
 import Routes             from './Routes';
-
 // Creates and configures an ExpressJS web server.
 class App {
 
@@ -21,7 +20,8 @@ class App {
 
   private crossDomain(): void {
     this.express.use(function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+      res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT,DELETE");
       next();
